e0008b0e9d59b033b6b0e17ad65b87ebb5bbffa2
Tefileo Stephens <TStephens@spartaglobal.com>
2020-02-05 16:51:42 +0000

    project folder setup

---
+++ Project5.py
@@ -0,0 +1,12 @@
+#Assignment for post class
+# Learning outcome: [variables, print, different data types]
+# ask and store the following in variables:
+ # Name, last_name, age, age_of_mother, 3 skills
+ # Print out the information in a formated manor
+ # Calculate age difference between response and mother
+ # Store skills in a list
+ # Print each skill in a formated way.
+# Definition of formated
+ # a little context text
+ # appropriate string formating like lower case or upper case, or other
+# assignment to variable
